0ec0f8352d920cf9a621968e49a003aa216ee4f1
maikimo <mike.p.monahan@gmail.com>
2018-06-21 00:01:47 -0400

    uh??

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = "AIzaSyDjAGBO-yMQ_dJZpho2JbtY_kLueEeNul0";
 
 // create a new component
 // component produces some html
 
 const App = (() => {
-  return <div>Hi!</div>
+  return <div>
+    < SearchBar /> 
+  </div>
 });
 
 // make sure it finds its way into the dom
