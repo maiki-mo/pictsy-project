cce428703be5133432fa85a15ea98a9f8aa1b583
maikimo <mike.p.monahan@gmail.com>
2018-06-30 08:38:33 -0400

    react stuff

--- src/components/checkboxes.js
+++ src/components/checkboxes.js
@@ -5,7 +5,7 @@ const Checkbox = ({ checkBoxes }) => {
   return (
   <div id="check-boxes">
     <br />
-    <input onChange={(e) => { this.viralCheckItem(e) } } style={{ display: 'inline-block', width: '40px' }} type="checkbox" id="viral" value='false' name="viral" /><label>Viral</label>
+    <input onChange={(e) => { viralCheckItem(e) } } style={{ display: 'inline-block', width: '40px' }} type="checkbox" id="viral" value='false' name="viral" /><label>Viral</label>
     <input onChange={(e) => { this.sfwCheckItem(e) } } style={{ display: 'inline-block', width: '40px' }} type="checkbox" value='false' name="sfw"/><label>SFW</label>
     <input onChange={(e) => { this.aniCheckItem(e) } } style={{ display: 'inline-block', width: '40px' }} type="checkbox" value='false' name="animated"/><label>Animated</label>
   </div>
